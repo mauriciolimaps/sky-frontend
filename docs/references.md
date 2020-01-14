e19848665ac6e01a0a4dea67a0bac27f151312a9
Mauricio Lima <github@mauriciolima.eti.br>
2020-01-13 21:54:10 -0300

    References update

--- docs/references.md
+++ docs/references.md
@@ -8,4 +8,14 @@ CSS Image size, how to fill, not stretch?
 https://stackoverflow.com/questions/11757537/css-image-size-how-to-fill-not-stretch
 
 
+
 https://devstore.io/js/gh-pages
+
+ritwickdey/vscode-live-server
+https://github.com/ritwickdey/vscode-live-server
+
+
+gulp-uglifycss
+https://www.npmjs.com/package/gulp-uglifycss
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,44 @@
-const gulp    = require('gulp')
-//const htmlmin = require('gulp-htmlmin');
-//const uglify = require('gulp-uglify')`
-const ghpages = require('gh-pages')
+const gulp      = require('gulp')
+const htmlmin   = require('gulp-htmlmin')
+const uglifycss = require('gulp-uglifycss')
+const uglify    = require('gulp-uglify')
+const del       = require('del')
+const ghpages   = require('gh-pages')
 
 gulp.task('html', function () {
     return gulp.src('source/frontend/*.html')
-//        .pipe(htmlmin({ 
-//            collapseWhitespace : true,
-//            ignorePath         : 'assets' 
-//        }))
+        .pipe(htmlmin({ 
+            collapseWhitespace : true,
+            ignorePath         : 'assets' 
+        }))
         .pipe(gulp.dest('dist'))
+})
+
+gulp.task('js', () => {
+    return gulp.src('source/frontend/js/*.js')
+//        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'))
 })
 
 gulp.task('css', () => {
     return gulp.src('source/frontend/css/*.css')
+        .pipe(uglifycss({
+            'maxLineLen'    : 80,
+            'uglyComments'  : true
+        }))
         .pipe(gulp.dest('dist/css'))
 })
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']), () => console.log('Clean done'))
+
+gulp.task('build', gulp.series('html', 'js', 'css'))
 
 gulp.task('deploy', function () {
     return ghpages.publish('dist', { add: true }, () => console.log('gh pages'))
 })
 
-gulp.task('default', gulp.series('html', 'css'))
+gulp.task('default', gulp.series('clean'), () => {
+    gulp.start('build')
+})
 
 

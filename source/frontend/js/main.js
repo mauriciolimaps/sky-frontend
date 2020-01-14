432508630521f95cb047e4f949de5062510e1257
Mauricio Lima <github@mauriciolima.eti.br>
2020-01-13 21:52:09 -0300

    Showing movies on initial layout

---
+++ source/frontend/js/main.js
@@ -0,0 +1,50 @@
+(function() {
+
+    function DOMLoaded()
+    {
+        buildPage()
+    }
+
+    async function buildPage()
+    {
+        if (!fetch) return
+
+        try
+        {
+            let card
+
+            const response = await fetch('/backend/movies/index.json')
+            const data = await response.json()
+            console.log(data)
+
+            const template = document.querySelector('#movie-card-template').content
+
+            const container = document.getElementById('container')
+            for(let movie of data[2].movies)
+            {
+                let titleNode
+
+                card = document.importNode(template, true)
+                titleNode = card.querySelector('.movie-title')
+                if (titleNode)
+                {
+                    titleNode.textContent = movie.title
+                }
+
+                imageNode = card.querySelector('.movie-header')
+                if (imageNode)
+                {
+                    imageNode.setAttribute('style', 'background-size:cover;background-position: 100% 100%;background-image:url(\'' + movie.images[0].url + '\')') 
+                }
+                
+                container.appendChild(card)
+            }
+        }
+        catch (e)
+        {
+            alert(e.message)
+        }
+    }
+
+    document.addEventListener('DOMContentLoaded', DOMLoaded)
+})()

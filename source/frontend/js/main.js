521c849d790c9f082e06d93fa9268228a9ddc5d3
Mauricio Lima <github@mauriciolima.eti.br>
2020-01-14 21:31:49 -0300

    Movies grouped by category

--- source/frontend/js/main.js
+++ source/frontend/js/main.js
@@ -13,32 +13,55 @@
         {
             let card
 
-            const response = await fetch('/backend/movies/index.json')
+            const response = await fetch('backend/movies/index.json')
             //const response = await fetch('http://sky-frontend.herokuapp.com/movies', {mode: 'no-cors'})
             const data = await response.json()
             console.log(data)
 
             const template = document.querySelector('#movie-card-template').content
 
-            const container = document.getElementById('container')
+            const categorized = {}
             for(let movie of data[2].movies)
             {
-                let titleNode
+                movie.categories
+                    .trim()
+                    .split(/\s*,\s*/)
+                    .map( (item) => {
+                        categorized[item] = (!categorized[item]) ? [] : categorized[item]
+                        categorized[item].push(movie) 
+                    })
+            }
+            console.log(categorized)
 
-                card = document.importNode(template, true)
-                titleNode = card.querySelector('.movie-title')
-                if (titleNode)
-                {
-                    titleNode.textContent = movie.title
-                }
+            const container = document.getElementById('container')
+            for(let category in categorized)
+            {
+                const title = document.createElement('h4')
+                title.textContent = category
 
-                imageNode = card.querySelector('.movie-header')
-                if (imageNode)
+                const categoryContainer = document.createElement('div')
+                const div = document.createElement('div')
+                div.appendChild(title)
+                div.appendChild(categoryContainer)
+
+                for(let movie of categorized[category])
                 {
-                    imageNode.setAttribute('style', 'background-size:cover;background-position: 100% 100%;background-image:url(\'' + movie.images[0].url + '\')') 
+                    const card = document.importNode(template, true)
+                    const titleNode = card.querySelector('.movie-title')
+                    if (titleNode)
+                    {
+                        titleNode.textContent = movie.title
+                    }
+    
+                    imageNode = card.querySelector('.movie-header')
+                    if (imageNode)
+                    {
+                        imageNode.setAttribute('style', 'background-size:cover;background-position: 100% 100%;background-image:url(\'' + movie.images[0].url + '\')') 
+                    }
+                    
+                    categoryContainer.appendChild(card)
                 }
-                
-                container.appendChild(card)
+                container.appendChild(div)
             }
         }
         catch (e)
